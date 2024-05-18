9666958121517530298c57d92c0ce505e345b1b0
hungsilver <hungsilvertq@gmail.com>
2024-05-18 16:16:17 +0700

    'dev'

--- src/main/java/com/example/demodeploy/controller/KhachHangController.java
+++ src/main/java/com/example/demodeploy/controller/KhachHangController.java
@@ -3,6 +3,7 @@ package com.example.demodeploy.controller;
 import com.example.demodeploy.entity.Khachhang;
 import com.example.demodeploy.repository.KhachHangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,17 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/khachhang")
 public class KhachHangController {
+
     @Autowired
     KhachHangRepository khachHangRepository;
-    @GetMapping
-    public List<Khachhang> findAllCustomers(){
-        return khachHangRepository.findAll();
+
+    @GetMapping("")
+    public ResponseEntity<?> findAllCustomers() {
+        return ResponseEntity.ok(khachHangRepository.findAll());
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello(){
+        return  ResponseEntity.ok("Hello!");
     }
 }
